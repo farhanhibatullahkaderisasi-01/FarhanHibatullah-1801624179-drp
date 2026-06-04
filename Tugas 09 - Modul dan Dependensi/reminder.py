ab977642c43b4a30fef5d4ae43afc4da10da0f79
Farhan Hibatullah <farhanhibatullahkaderisasi@gmail.com>
2026-06-04 10:15:18 +0700

    Mengganti folder tugas 09

--- Tugas 09 - Modul dan Dependensi/reminder.py
+++
@@ -1,7 +0,0 @@
-def cek_stres(tugas):
-
-    if len(tugas) >= 5:
-        print("\n⚠️ Banyak tugas terdeteksi!")
-        print("Jangan lupa istirahat ya.")
-    else:
-        print("\nBeban tugas masih aman.")
