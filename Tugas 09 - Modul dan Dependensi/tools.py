4950180b050855e6834555fe0dd3b5a483687d37
aura hanaya <aurahanaya22@gmail.com>
2026-06-05 22:49:06 +0700

    Add menu navigation in tools.py

--- Tugas 09 - Modul dan Dependensi/tools.py
+++ Tugas 09 - Modul dan Dependensi/tools.py
@@ -6,42 +6,42 @@ from manager.stress_reminder import stress_reminder
 
 def display_menu(user):
 
-    print()
-    print("🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤")
-    print(f"Hello,{user}! chosee a feature to use!✨")
-    print("🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤")
+    print ()
+    print ("🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤")
+    print (f"Hello, {user}! Choose a feature you want to use!✨")
+    print ("🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤")
 
-    print("1. Task Tracker 🧮")
-    print("2. Priority Task ⚡")
-    print("3. Workload Monitor 📊")
-    print("4. Coompletion Progress ✅")
-    print("5. Stress Reminder ‼️")
-    print("6. Exit ❌")
+    print ("1. Task Tracker 🧮")
+    print ("2. Priority Task ⚡")
+    print ("3. Workload Monitor📊 ")
+    print ("4. Completion Progress 📈")
+    print ("5. Stress Reminder 🧠")
+    print ("6. Exit 🚪")
 
-    print("🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤")
+    print ("🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤")
 
-def select_menu(menu):
+def select_menu (menu):
     
     if menu == "1":
         task_tracker()
 
     elif menu == "2":
         priority_task()
-    
+
     elif menu == "3":
         workload_monitor()
 
     elif menu == "4":
         completion_progress()
-    
+
     elif menu == "5":
         stress_reminder()
 
     elif menu == "6":
-        print("\nExiting Program...")
+        print ("\nExiting Program... Goodbye! 👋")
         return True
     
     else:
-        print("\n⚠️ Invalid Menu!")
+        print ("\n⚠️ Invalid Menu!")
 
     return False
