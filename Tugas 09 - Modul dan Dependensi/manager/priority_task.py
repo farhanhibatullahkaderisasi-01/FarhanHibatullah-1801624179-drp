d5523e66477237079dd66ba46bc61e291ae1c60a
aura hanaya <aurahanaya22@gmail.com>
2026-06-17 23:56:08 +0700

    Menambahkan CRUD Task Tracker dan Priority Task

--- Tugas 09 - Modul dan Dependensi/manager/priority_task.py
+++ Tugas 09 - Modul dan Dependensi/manager/priority_task.py
@@ -1,9 +1,65 @@
-def priority_task():
+import sqlite3
 
-    print("\n ⚡ Priority Task ⚡")
-    print("🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤")
-    print("This features helps user")
-    print("organize tasks based on priority.")
-    print("🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤")
+def create_priority ():
 
-    input("\nPress Enter to return...")
+    kategori = input ("Masukkan kategori prioritas:")
+
+    conn = sqlite3.connect ("taskmate.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""INSERT INTO priority (kategori) VALUES (?)""", (kategori,))
+
+    conn.commit()
+    conn.close()
+
+    print("✅Kategori prioritas berhasil ditambahkan!")
+
+def read_priority ():  
+
+    conn = sqlite3.connect ("taskmate.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM priority")
+    priorities = cursor.fetchall()
+
+    print("\n📋 Daftar Prioritas:")
+
+    if len(priorities) == 0:
+        print("Belum ada kategori prioritas yang ditambahkan.")
+
+    else:
+        for priority in priorities:
+            print (priority)
+
+    conn.close()
+
+def update_priority (): 
+
+    id_priority = input ("Masukkan ID kategori prioritas yang ingin diupdate: ")  
+    kategori_baru = input ("Masukkan kategori prioritas baru: ")  
+
+    conn = sqlite3.connect ("taskmate.db") 
+    cursor = conn.cursor()  
+
+    cursor.execute("""UPDATE priority SET kategori = ? WHERE id_priority = ?""", (kategori_baru, id_priority))
+
+    conn.commit() 
+    conn.close()    
+
+    print("✅Kategori prioritas berhasil diupdate!")
+
+def delete_priority (): 
+
+    id_priority = input ("Masukkan ID kategori prioritas yang ingin dihapus: ") 
+
+    conn = sqlite3.connect ("taskmate.db") 
+    cursor = conn.cursor() 
+
+    cursor.execute("""DELETE FROM priority WHERE id_priority = ?""", (id_priority,)) 
+
+    conn.commit()
+    conn.close() 
+
+    print("🗑️Kategori prioritas berhasil dihapus!")
+
+   
--- Tugas 09 - Modul dan Dependensi/manager/task_tracker.py
+++ Tugas 09 - Modul dan Dependensi/manager/task_tracker.py
@@ -1,9 +1,66 @@
-def task_tracker():
+import sqlite3
 
-    print("\n🧮 Task Tracker 🧮")
-    print("🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤")
-    print("This features helps user")
-    print("record and monitor tasks.")
-    print("🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤🟤")
+def create_task (): 
 
-    input("\nPress Enter to return...")
+    nama_task = input("Masukkan nama task: ")
+    deadline = input ("Masukkan deadline task:")
+    status = input ("Masukkan status task (Belum selesai/selesai):")
+
+    conn = sqlite3.connect ("taskmate.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""INSERT INTO task (nama_task, deadline, status) VALUES (?, ?, ?)""", (nama_task, deadline, status))
+    
+    conn.commit()
+    conn.close()
+
+    print("✅Task berhasil ditambahkan!")
+
+
+def read_task ():
+
+    conn = sqlite3.connect ("taskmate.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM task")
+    tasks = cursor.fetchall()
+
+    print("\n📋 Daftar Task:")
+
+    if len(tasks) == 0:
+        print("Belum ada task yang ditambahkan.")
+
+    else:
+        for task in tasks:
+            print (task)
+
+    conn.close()
+
+def update_task ():
+
+    id_task = input ("Masukkan ID task yang ingin diupdate: ")
+    status_baru = input ("Masukkan status baru (Belum selesai/selesai): ")
+
+    conn = sqlite3.connect ("taskmate.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""UPDATE task SET status = ? WHERE id_task = ?""", (status_baru, id_task))
+
+    conn.commit()
+    conn.close()
+
+    print("✅Status task berhasil diupdate!")
+
+def delete_task ():
+
+    id_task = input ("Masukkan ID task yang ingin dihapus: ")
+
+    conn = sqlite3.connect ("taskmate.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""DELETE FROM task WHERE id_task = ?""", (id_task,))
+
+    conn.commit()
+    conn.close()
+
+    print("🗑️Task berhasil dihapus!")
