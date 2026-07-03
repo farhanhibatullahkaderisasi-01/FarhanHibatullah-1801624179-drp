72600443ce76d17d57008307a09d89b2d36d0242
Nur Muzakiyah <nurmuzakiyah12@gmail.com>
2026-07-03 11:27:43 +0700

    Menambahkan fitur Task Statistics dan Insight Kesimpulan Tugas 13

--- Tugas 09 - Modul dan Dependensi/manager/task_statistics.py
+++ Tugas 09 - Modul dan Dependensi/manager/task_statistics.py
@@ -22,4 +22,26 @@ def task_statistics():
     """)
     task_belum_selesai = cursor.fetchone()[0]
 
+    if total > 0:
+        persentase = (task_selesai / total) * 100
+    else:
+        persentase = 0
+
+    print("\n===== TASK STATISTICS =====")
+    print(f"Total Task          : {total}")
+    print(f"Task Selesai        : {task_selesai}")
+    print(f"Task Belum Selesai  : {task_belum_selesai}")
+    print(f"Persentase Selesai  : {persentase:.2f}%")
+
+    print("\nKesimpulan:")
+
+    if persentase == 100:
+        print("Semua tugas sudah selesai.")
+    elif persentase >= 70:
+        print("Sebagian besar tugas sudah selesai.")
+    elif persentase >=40:
+        print("Progres tugas sedang berjalan.")
+    else:
+        print("Masih banyak tugas yang belum selesai.")
+
     conn.close()
